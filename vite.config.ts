acdca051e7c7dd695503ef159048356e896e294e
suntongwei <suntongwei@ctjsoft.com>
2026-08-24 15:45:24 +0800

    完成 coco food map 第一版

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import vueJsx from "@vitejs/plugin-vue-jsx";
 import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/cocofoodmap/",
+  base: "/coco-food-map/",
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
