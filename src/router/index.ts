02b83fa10f17b0ff3a3008fb825e69efed318b02
suntongwei <suntongwei@ctjsoft.com>
2026-08-27 16:35:51 +0800

    零食list更新

--- src/router/index.ts
+++ src/router/index.ts
@@ -10,6 +10,11 @@ const routes: Array<RouteRecordRaw> = [
     name: "index",
     component: () => import("@/views/index/index"),
   },
+  {
+    path: "/snacks",
+    name: "snacks",
+    component: () => import("@/views/snacks/index"),
+  },
 ];
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
